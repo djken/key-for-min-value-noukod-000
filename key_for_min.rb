1a008ba824a7aa138348347d343617df576a7205
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-10 17:15:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 ikea = {:chair => 25, :table => 85, :mattress => 450}
 def key_for_min_value(name_hash)
   smallest_key = nil
-  smallest_value = 4
+  smallest_value = 0
   
   name_hash.collect do |key, value|
     if smallest_value > value || smallest_value == 0
